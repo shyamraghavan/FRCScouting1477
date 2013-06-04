5e62a3257bb68406aa40853b552341d630828525
Shyam Raghavan <shyam.raghavan.1@gmail.com>
2013-06-04 16:54:50 -0500

    created background_activity (misnomer), send_data activity, and interaction.

---
+++ src/com/texastorque/TorqueFRCScouting/BackgroundActivity.java
@@ -0,0 +1,29 @@
+package com.texastorque.TorqueFRCScouting;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Shyam Raghavan
+ * Date: 6/3/13
+ * Time: 1:28 PM
+ */
+public class BackgroundActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public void buttonClick(View view){
+        Intent sendDataIntent = new Intent(this,SendData.class);
+
+        EditText user_entered_message = (EditText) findViewById(R.id.text_entry_box);
+
+        sendDataIntent.putExtra("com.texastorque.TorqueFRCScouting.user_entered_message",user_entered_message.getText());
+
+        startActivity(sendDataIntent);
+    }
+}
---
+++ src/com/texastorque/TorqueFRCScouting/SendData.java
@@ -0,0 +1,16 @@
+package com.texastorque.TorqueFRCScouting;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Shyam Raghavan
+ * Date: 6/4/13
+ * Time: 4:51 PM
+ */
+public class SendData extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
