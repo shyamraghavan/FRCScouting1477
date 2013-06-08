63a863005573c9f12508f57eed0b6777b0ed933c
Shyam Raghavan <shyam.raghavan.1@gmail.com>
2013-06-07 21:49:23 -0500

    reorder of BackgroundActivity, still unworking.

--- src/com/texastorque/TorqueFRCScouting/BackgroundActivity.java
+++ src/com/texastorque/TorqueFRCScouting/BackgroundActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
 /**
  * Created with IntelliJ IDEA.
@@ -16,10 +17,17 @@ import android.widget.Spinner;
  */
 
 public class BackgroundActivity extends Activity {
+
+    private SpinnerAdapter arrayAdapter;
+    private Spinner spinner;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setup(savedInstanceState);
+
+        //spinner.setAdapter(arrayAdapter);
+
         setContentView(R.layout.background_activity);
     }
 
@@ -36,10 +44,8 @@ public class BackgroundActivity extends Activity {
     public void setup(Bundle savedInstanceState){
         super.setTitle(R.string.app_name);
 
-        Spinner spinner = (Spinner) findViewById(R.id.state_spinner);
+        spinner = (Spinner) findViewById(R.id.state_spinner);
 
-        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(this,R.array.spinner_text_array,R.id.state_spinner);
-
-        spinner.setAdapter(arrayAdapter);
+        arrayAdapter = ArrayAdapter.createFromResource(this,R.array.spinner_text_array,R.id.state_spinner);
     }
 }
